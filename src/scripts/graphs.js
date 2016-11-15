84a97957405bb6c7ed9480e4319416d5516b3929
Cody Henderson <cody@cthenderson.com>
2016-11-14 19:37:54 -0600

    Begin graphing work. Clock offset between sender/receiver needs to be fixed

--- src/scripts/graphs.js
+++ src/scripts/graphs.js
@@ -22,3 +22,49 @@ function generateLineGraph(data, container) {
         .attr("height", 500)
         .append("g");
 }
+
+function process10Packets(selector, sender, receiver) {
+    var packets = TV.db.getCollection('pairs').chain().simplesort('senttime').limit(10).data();
+
+    var series = [];
+    var min=Number.MAX_VALUE;
+    var max = 0;
+    for (var i=0; i<packets.length; i++) {
+        var sendPos = (packets[i].fromip == sender)?0:1;
+        var recPos = (packets[i].fromip == sender)?1:0;
+        min = (min<packets[i].senttime)?min:packets[i].senttime;
+        max = (max>packets[i].senttime)?max:packets[i].senttime;
+        min = (min<packets[i].rectime)?min:packets[i].rectime;
+        max = (max>packets[i].rectime)?max:packets[i].rectime;
+        series.push([
+            {"x":packets[i].senttime,
+            "y":sendPos},
+            {"x":packets[i].rectime,
+            "y":recPos}
+        ]);
+    }
+
+    var vis = d3.select(selector),
+        WIDTH = 500,
+        HEIGHT = 500,
+        MARGINS = {
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 50
+        };
+
+    var xScale = d3.scale.linear().range([MARGINS.left, WIDTH-MARGINS.right]).domain([min, max]);
+    var yScale = d3.scale.linear().range([HEIGHT-MARGINS.top, MARGINS.bottom]).domain([0,1]);
+    var xAxis = d3.svg.axis().scale(xScale);
+    var yAxis = d3.svg.axis().scale(yScale).orient("left");
+    //var xAxis = d3.axisBottom().scale(xScale);
+    //var yAxis = d3.axisLeft().scale(yScale);
+    vis.append("svg:g").attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")").call(xAxis);
+    vis.append("svg:g").attr("transform", "translate(" + (MARGINS.left) + ",0)").call(yAxis);
+
+    var lineGen = d3.svg.line().x(function(d) {return xScale(d.x)}).y(function(d) {return yScale(d.y)});
+    for (var i=0; i<series.length; i++) {
+        vis.append('svg:path').attr('d', lineGen(series[i])).attr('stroke-width', 2).attr('stroke', 'blue').attr('fill', 'none');
+    }
+}
