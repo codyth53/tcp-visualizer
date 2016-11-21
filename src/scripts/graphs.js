74b695f2f0a60c3c3a0df4ad6556aaa5b402cbd6
Cody Henderson <cody@cthenderson.com>
2016-11-20 20:39:43 -0600

    Added zooming ability to graph

--- src/scripts/graphs.js
+++ src/scripts/graphs.js
@@ -24,7 +24,7 @@ function generateLineGraph(data, container) {
 }
 
 function process10Packets(selector, sender, receiver) {
-    var packets = TV.db.getCollection('pairs').chain().simplesort('sendertime').data();
+    var packets = TV.db.getCollection('pairs').chain().simplesort('sendertime')/*.limit(200)*/.data();
 
     var series = [];
     var min=Number.MAX_VALUE;
@@ -46,8 +46,7 @@ function process10Packets(selector, sender, receiver) {
         ]);
     }
 
-    var vis = d3.select(selector),
-        WIDTH = 1000,
+    var WIDTH = 1000,
         HEIGHT = 200,
         MARGINS = {
             top: 20,
@@ -60,16 +59,41 @@ function process10Packets(selector, sender, receiver) {
     var yScale = d3.scale.linear().range([HEIGHT-MARGINS.top, MARGINS.bottom]).domain([0,1]);
     var xAxis = d3.svg.axis().scale(xScale);
     var yAxis = d3.svg.axis().scale(yScale).orient("left");
-    //var xAxis = d3.axisBottom().scale(xScale);
-    //var yAxis = d3.axisLeft().scale(yScale);
-    vis.append("svg:g").attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")").call(xAxis);
-    vis.append("svg:g").attr("transform", "translate(" + (MARGINS.left) + ",0)").call(yAxis);
+    var zoom = d3.behavior.zoom().x(xScale)/*.y(yScale)*//*.scaleExtent([1,10])*/.on("zoom", zoomed);
+
+    var vis = d3.select(selector).call(zoom).append("svg")
+        .call(zoom)
+        .attr("width", WIDTH + MARGINS.left + MARGINS.right)
+        .attr("height", HEIGHT + MARGINS.top + MARGINS.bottom)
+        .append("g")
+        .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.top + ")")
+        .call(zoom);
+
+    vis.append("svg:g").attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")").attr("class", "x axis").call(xAxis);
+    vis.append("svg:g").attr("transform", "translate(" + (MARGINS.left) + ",0)").attr("class", "y axis").call(yAxis);
 
     var lineGen = d3.svg.line().x(function(d) {return xScale(d.x)}).y(function(d) {return yScale(d.y)});
-    for (var i=0; i<series.length; i++) {
+    /*for (var i=0; i<series.length; i++) {
         var color = 'blue';
         if (series[i][0].y == 1)
             color = 'red';
         vis.append('svg:path').attr('d', lineGen(series[i])).attr('stroke-width', 2).attr('stroke', color).attr('fill', 'none');
+    }*/
+    vis.selectAll('.line')
+        .data(series)
+        .enter()
+        .append('path')
+        .attr("class", "line")
+        .attr('stroke-width', 1)
+        .attr('stroke', function(d,i){
+            return series[i][0].y==1?'red':'blue';
+        })
+        .attr('d', lineGen);
+
+    function zoomed() {
+        vis.select(".x.axis").call(xAxis);
+        //vis.select(".y.axis").call(yAxis);
+        vis.selectAll('path.line').attr('d', lineGen);
+        console.log("I zoomed");
     }
 }
